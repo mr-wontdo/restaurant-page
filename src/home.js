479901875d694ae69f5e8f7c443f3596cabb6771
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-10-15 15:31:05 -0700

    Add home.js

---
+++ src/home.js
@@ -0,0 +1,51 @@
+export default function createHome() {
+    const content = document.createElement('div');
+    content.classList.add('content-container');
+
+    // Content one
+    const contentOne = document.createElement('p');
+    contentOne.textContent = 'The steaks have never been higher.'
+
+    // Content two
+    const contentTwo = document.createElement('div');
+
+    const hoursHeader = document.createElement('h3');
+    hoursHeader.textContent = 'Hours'
+
+    const hoursContainer = document.createElement('div');
+    const sunday = document.createElement('p');
+    sunday.textContent = 'Sunday: 8AM - 10PM';
+    const monday = document.createElement('p');
+    monday.textContent = 'Monday: 11AM - 8PM';
+    const tuesday = document.createElement('p');
+    tuesday.textContent = 'Tuesday: 11AM - 8PM';
+    const wednesday = document.createElement('p');
+    wednesday.textContent = 'Wednesday: 11AM - 8PM';
+    const thursday = document.createElement('p');
+    thursday.textContent = 'Thursday: 11AM - 8PM';
+    const friday = document.createElement('p');
+    friday.textContent = 'Friday: 8AM - 10PM';
+    const saturday = document.createElement('p');
+    saturday.textContent = 'Saturday: 8AM - 10PM';
+
+    // Content three
+    const contentThree = document.createElement('p');
+    contentThree.textContent = 'Order online or visit us!';
+
+    // Append
+    document.querySelector('#content').appendChild(content);
+    content.appendChild(contentOne);
+    content.appendChild(contentTwo);
+    content.appendChild(contentThree);
+
+    contentTwo.appendChild(hoursHeader);
+    contentTwo.appendChild(hoursContainer);
+    hoursContainer.appendChild(sunday);
+    hoursContainer.appendChild(monday);
+    hoursContainer.appendChild(tuesday);
+    hoursContainer.appendChild(wednesday);
+    hoursContainer.appendChild(thursday);
+    hoursContainer.appendChild(friday);
+    hoursContainer.appendChild(saturday);
+
+}
