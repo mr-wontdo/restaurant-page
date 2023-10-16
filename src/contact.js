1dfd2c2cacd8048ed7af2cef0c6926f4a0ffd85e
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-10-15 19:57:29 -0700

    Update contact container styling and add location image

--- src/contact.js
+++ src/contact.js
@@ -3,12 +3,17 @@ export default function createContact() {
     content.classList.add('contact-container');
 
     const phoneNumber = document.createElement('p');
-    phoneNumber.textContent = '(515) 555-5555';
+    phoneNumber.textContent = '📞 (515) 555-5555';
+
     const address = document.createElement('p');
-    address.textContent = 'Hollywood Boulevard 42, Los Angeles, USA'
+    address.textContent = '📍 Hollywood Boulevard 42, Los Angeles, USA';
+
+    const image = document.createElement('img');
+    image.src = 'images/restaurant-location.png';
 
     // Append
     document.querySelector('#content').appendChild(content);
     content.appendChild(phoneNumber);
     content.appendChild(address);
+    content.appendChild(image);
 }
